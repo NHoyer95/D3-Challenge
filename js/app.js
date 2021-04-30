bbaa87e7d05ce67eb807973f6a4fa044dc89ff78
NHoyer95 <hoyer047@umn.edu>
2021-04-30 14:23:28 -0500

    Populate the datapoints as markers

--- js/app.js
+++ js/app.js
@@ -52,7 +52,19 @@ d3.csv("/data/data.csv").then(function(censusData) {
 
     chartGroup.append("g")
         .call(yAxis);
-        
+
+    // Create the markers from the poverty and healthcare data points
+    var dataPoints = chartGroup.selectAll("circle")
+        .data(censusData)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xLinearScale(d.poverty))
+        .attr("cy", d => yLinearScale(d.healthcare))
+        .attr("r", 15)
+        .attr("fill", "blue")
+        .attr("opacity", ".5")
+        .attr("stroke", "black")
+
 });
 
     
